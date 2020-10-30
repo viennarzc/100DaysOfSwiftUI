cefbfc7eb1657223cfaf42b36780d48cba817558
Viennarz Curtiz <viennarzc@gmail.com>
2020-10-30 16:42:51 +0800

    Add Views and Logics

--- Day16/WeSplit/WeSplit/ContentView.swift
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+  @State private var checkAmount = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 2
+  
+  let tipPercentages = [10, 15, 20, 25, 0]
+  
+  var totalPerPerson: Double {
+    let peopleCount = Double(numberOfPeople + 2)
+    let tipSelection = Double(tipPercentages[tipPercentage])
+    
+    let orderAmount = Double(checkAmount) ?? 0
+    
+    let stringValue = "0.5"
+    let doubleValue = Double(stringValue)
+    
+    let tipValue = orderAmount / 100 * tipSelection
+    let grandTotal = orderAmount + tipValue
+    let amountPerPerson = grandTotal / peopleCount
+    
+    return amountPerPerson
+
+  }
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount", text: $checkAmount)
+            .keyboardType(.decimalPad )
+          
+          Picker("Number of People", selection: $numberOfPeople) {
+            ForEach(2 ..< 100) {
+              Text("\($0) people")
+            }
+          }
+        }
+        
+        Section(header: Text("How much tip do you want to leave?")) {
+          
+          Picker("Tip Percentage", selection: $tipPercentage) {
+            ForEach(0 ..< tipPercentages.count) {
+              Text("\(self.tipPercentages[$0])%")
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+          
+        }
+        
+        Section {
+          Text("$\(totalPerPerson)")
+        }
+        
+      }
+      .navigationBarTitle("WeSplit")
+      
     }
+  }
+  
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    Group {
+      ContentView()
     }
+  }
 }
