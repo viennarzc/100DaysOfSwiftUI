752ff39cdd3609fb751090ec312efa40e50d3707
Viennarz Curtiz <viennarzc@gmail.com>
2020-11-25 23:10:04 +0800

    Project 3 of Day 23

---
+++ Day23_Project3/Day23_Project3/Day23_Project3App.swift
@@ -0,0 +1,17 @@
+//
+//  Day23_Project3App.swift
+//  Day23_Project3
+//
+//  Created by Viennarz Curtiz on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct Day23_Project3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
