db16f5e6ead0881a7cdf46665cf40b0e4a14f9e1
Viennarz Curtiz <viennarzc@gmail.com>
2020-12-30 20:14:43 +0800

    Create Project Day 26 - BetterRest

---
+++ Day26/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Viennarz Curtiz on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
